d2fc09f1c42c394c201048818c7b68367a0ba190
marvellous-omoruyi <marvellousomoruyi8@gmail.com>
2025-08-29 16:56:42 +0100

    Update level_scene.gd

--- Scenes/Levels/level_scene.gd
+++ Scenes/Levels/level_scene.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-# This is the corrected path. Your player is inside the "TileMap" node.
-@onready var player = $TileMap/player
+# The path must match the node's name exactly, including capitalization.
+@onready var player = $Player
 @onready var hud = $HUD
 
 func _ready():
-	# Set the initial health on the HUD when the level loads
+	# Now that the script can find "Player", this code will execute correctly.
 	hud.set_max_health(player.health)
-	
-	# Connect the player's health_changed signal to the HUD's update_health function
 	player.health_changed.connect(hud.update_health)
